ac1fbf95489ada4b99d9894d909d8b9ab2a90582
nightwatch41 <nightwatch41@cbf85e06-923c-0410-8310-49c9b69bbbde>
2007-10-16 21:45:52 +0000

    changes to kamaitachi - WILL NOT WORK but better than before

--- kamaitachi/kamaitachi.c
+++ kamaitachi/kamaitachi.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <sys/ptrace.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -10,30 +11,45 @@
 
 #define KERNEL_OFFSET   0xc010378c
 
+#define MIN(x,y)        ((x) < (y) ? (x) : (y))
+
+#define THINGY_COUNT            4
+#define THINGY_BOOTLOADER_CODE  0
+#define THINGY_BOOT_PICTURE     1
+#define THINGY_KERNEL           2
+#define THINGY_FONT             3
+
 struct allocator_data {
     uint32_t total_size; 
     uint32_t allocated_data;
     uint32_t allocated_data_phys;
 };
 
-/* These are (offset in kernel, size) pairs. */
-struct bootloader_data {
-    uint32_t phys_start;        /* 0 */
-    uint32_t code;
-    uint32_t code_size;         /* 8 */
-    uint32_t img;
-    uint32_t img_size;          /* 16 */
-    uint32_t kernel;            /* 20 */
-    uint32_t kernel_size;       /* 24 */
-    uint32_t ramdisk;
-    uint32_t ramdisk_size;
-    uint32_t boot_args;
-    uint32_t boot_args_size;
+/* A "thingy" is a piece of data that will be injected into the Darwin kernel
+ * and which the bootloader has access to. In order to facilitate this, the
+ * bootloader needs to know the thingies' physical addresses, so they are
+ * stored here. */
+struct thingy {
+    uint32_t virt_addr;
+    uint32_t phys_addr;
+    uint32_t size;
 };
 
-extern uint8_t *boot_data, *loader_data, *allocator_data;
-extern uint32_t boot_len, loader_code_len, loader_len;
-extern uint32_t allocator_code_len, allocator_len;
+struct thingy_info {
+    char *path;
+    char *name;
+};
+
+extern void *boot_data, *allocator_data, *loader_data;
+extern uint32_t boot_len, allocator_code_len, allocator_len, loader_len;
+extern uint32_t loader_code_len;
+
+const struct thingy_info thingy_info[THINGY_COUNT] = {
+    [THINGY_BOOTLOADER_CODE] = { NULL, "the bootloader code" },
+    [THINGY_BOOT_PICTURE] = { "bootloader-image.tga" , "the boot picture" },
+    [THINGY_KERNEL] = { "vmlinuz", "the Linux kernel" },
+    [THINGY_FONT] = { "bootloader-font.tga", "the bootloader font" }
+};
 
 void inject(int fd, void *data, uint32_t len, uint32_t *kaddr, uint32_t *klen, 
     char *desc)
@@ -59,61 +75,92 @@ void inject(int fd, void *data, uint32_t len, uint32_t *kaddr, uint32_t *klen,
 
 int main()
 {
-    int fd;
-    uint8_t *img, *kernel;
-    uint32_t img_size, kernel_size, total_size;
+    char *path;
+    int fd, kfd, i;
+    uint8_t *ptr, *optr;
+    uint32_t total_size = 0, phys_addr, left, written;
     struct allocator_data adata;
-    struct bootloader_data data;
     struct stat sb;
+    struct thingy thingies[THINGY_COUNT];
 
-    data.ramdisk_size = 0;
-    data.boot_args_size = 0;
+    /* Die here rather than crashing in the bootloader if the stride of the
+     * thingies array is not unit... */
+    assert(sizeof(thingies) == sizeof(struct thingy) * THINGY_COUNT);
 
-    fprintf(stderr, "Loading bootloader image... ");
-    assert((fd = open(PKGDATADIR "/bootloader-image.tga", O_RDONLY, 0)) >= 0);
-    fstat(fd, &sb);
-    img = malloc(img_size = sb.st_size); 
-    assert(read(fd, img, img_size) == img_size);
-    close(fd);
-    fprintf(stderr, "%d bytes loaded.\n", (int)(sb.st_size));
+    /* Calculate the size of the space that needs to be allocated in the Darwin
+     * kernel. */
+    for (i = 0; i < THINGY_COUNT; i++) {
+        fprintf(stderr, "Calculating size of %s... ", thingy_info[i].name);
 
-    fprintf(stderr, "Loading Linux kernel... ");
-    assert((fd = open(PKGDATADIR "/vmlinuz", O_RDONLY, 0)) >= 0);
-    fstat(fd, &sb);
-    kernel = malloc(kernel_size = sb.st_size); 
-    assert(read(fd, kernel, kernel_size) == kernel_size);
-    close(fd);
-    fprintf(stderr, "%d bytes loaded.\n", (int)(sb.st_size));
+        if (thingy_info[i].path) {
+            asprintf(&path, "%s%s", PKGDATADIR, thingy_info[i].path);
+            assert(!stat(path, &sb));
+            free(path);
+            thingies[i].size = sb.st_size;
+        } else
+            thingies[i].size = boot_len;    /* bootloader code */
 
-    total_size = boot_len + img_size + kernel_size;
+        total_size += thingies[i].size;
+
+        fprintf(stderr, "%d B.\n", (int)(thingies[i].size));
+    }
 
     fprintf(stderr, "Injecting allocator (stage 1) stub into kernel... ");
-    assert((fd = open("/dev/kmem", O_RDWR, 0)) >= 0);
-    assert(lseek(fd, KERNEL_OFFSET, SEEK_SET) == KERNEL_OFFSET);
-    assert(write(fd, allocator_data, allocator_len) == allocator_len);
-    assert(lseek(fd, KERNEL_OFFSET + allocator_code_len, SEEK_SET) ==
+    assert((kfd = open("/dev/kmem", O_RDWR, 0)) >= 0);
+    assert(lseek(kfd, KERNEL_OFFSET, SEEK_SET) == KERNEL_OFFSET);
+    assert(write(kfd, allocator_data, allocator_len) == allocator_len);
+    assert(lseek(kfd, KERNEL_OFFSET + allocator_code_len, SEEK_SET) ==
         KERNEL_OFFSET + allocator_code_len);
-    assert(write(fd, &total_size, sizeof(uint32_t)) == sizeof(uint32_t)); 
+    assert(write(kfd, &total_size, sizeof(uint32_t)) == sizeof(uint32_t)); 
     fprintf(stderr, "%d bytes written ok.\n", allocator_len);
 
     fprintf(stderr, "Running allocator... ");
     ptrace(PT_ATTACH, 0, 0, 0);     /* runs allocator */
-    assert(lseek(fd, KERNEL_OFFSET + allocator_code_len, SEEK_SET) ==
+    assert(lseek(kfd, KERNEL_OFFSET + allocator_code_len, SEEK_SET) ==
         KERNEL_OFFSET + allocator_code_len);
-    assert(read(fd, &adata, sizeof(struct allocator_data)) == sizeof(struct
+    assert(read(kfd, &adata, sizeof(struct allocator_data)) == sizeof(struct
         allocator_data)); 
-    fprintf(stderr, "%d bytes allocated in kernel space at $%08x/%08x ok.\n",
+    fprintf(stderr, "ok, %d B at 0x%08x v, 0x%08x p.\n",
         (int)(adata.total_size), (uint32_t)(adata.allocated_data),
         (uint32_t)(adata.allocated_data_phys));
 
-    data.phys_start = adata.allocated_data_phys;
-    assert(lseek(fd, adata.allocated_data, SEEK_SET) == adata.allocated_data);
+    phys_addr = adata.allocated_data_phys;
+    assert(lseek(kfd, adata.allocated_data, SEEK_SET) == adata.allocated_data);
 
-    inject(fd, boot_data, boot_len, &(data.code), &(data.code_size),
-        "bootloader code"); /* code must be first */
-    inject(fd, img, img_size, &data.img, &data.img_size, "boot image");
-    inject(fd, kernel, kernel_size, &data.kernel, &data.kernel_size,
-        "Linux kernel");
+    /* Inject all the thingies into the kernel. */
+    for (i = 0; i < THINGY_COUNT; i++) {
+        fprintf(stderr, "Injecting %s... ", thingy_info[i].name);
+
+        thingies[i].virt_addr = lseek(kfd, 0, SEEK_CUR); 
+        thingies[i].phys_addr = phys_addr;
+
+        phys_addr += thingies[i].size;
+
+        if (thingy_info[i].path) {
+            asprintf(&path, "%s%s", PKGDATADIR, thingy_info[i].path);
+            assert((fd = open(path, O_RDONLY, 0)) >= 0); 
+            free(path);
+            assert(optr = mmap(NULL, thingies[i].size, PROT_READ, MAP_FILE, fd,
+                0));
+            ptr = optr;
+        } else
+            ptr = boot_data;        /* bootloader code */
+
+        left = thingies[i].size;
+
+        while (left) {
+            assert((written = write(kfd, ptr, left)) > 0);
+            left -= written; ptr += written;
+        }
+
+        fprintf(stderr, "ok, %d B at 0x%08x v, 0x%08x p.\n", (int)(
+            thingies[i].size), thingies[i].virt_addr, thingies[i].phys_addr);
+
+        if (thingy_info[i].path) {
+            munmap((caddr_t)optr, thingies[i].size);
+            close(fd);
+        }
+    }
 
     fprintf(stderr, "Injecting loader (stage 2) stub into kernel... ");
     assert((fd = open("/dev/kmem", O_WRONLY, 0)) >= 0);
@@ -121,19 +168,11 @@ int main()
     assert(write(fd, loader_data, loader_len) == loader_len);
     assert(lseek(fd, KERNEL_OFFSET + loader_code_len, SEEK_SET) ==
         KERNEL_OFFSET + loader_code_len);
-    assert(write(fd, &data, sizeof(struct bootloader_data)) ==
-        sizeof(struct bootloader_data)); 
+    assert(write(fd, thingies, sizeof(struct thingy) * THINGY_COUNT) ==
+        sizeof(struct thingy) * THINGY_COUNT); 
     fprintf(stderr, "%d bytes written ok.\n", loader_len);
 
-    FILE *f;
-    f = fopen("/tmp/bld", "w");
-    fwrite(&data, sizeof(struct bootloader_data), 1, f);
-    fclose(f);
-
-    return 0;
-
-    fprintf(stderr, "Starting bootloader at $%08x...\n", (uint32_t)(
-        data.code));
+    fprintf(stderr, "Starting bootloader...\n");
     ptrace(PT_ATTACH, 0, 0, 0);
 
     return 1;
